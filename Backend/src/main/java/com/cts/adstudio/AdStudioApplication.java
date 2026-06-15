f9ad2de5b4c9bca5998f39e16599c44e75297711
chaitanya-427 <kadalichaitanya.nagasatyacharan@cognizant.com>
2026-06-15 17:27:47 +0530

    initial

---
+++ Backend/src/main/java/com/cts/adstudio/AdStudioApplication.java
@@ -0,0 +1,13 @@
+package com.cts.adstudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdStudioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdStudioApplication.class, args);
+	}
+
+}
