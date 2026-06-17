a14d28347cefd4913c43c880905f5c0f35980aa5
Vishnu <ChilakalaVishnuvardhan.Reddy@cognizant.com>
2026-06-17 14:47:09 +0530

    completed advertiser and campaign module

---
+++ Backend/src/main/java/com/cts/adstudio/advertiser/repository/CampaignBriefRepository.java
@@ -0,0 +1,15 @@
+package com.cts.adstudio.advertiser.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cts.adstudio.advertiser.entity.CampaignBrief;
+
+@Repository
+public interface CampaignBriefRepository extends JpaRepository<CampaignBrief, Integer> { 
+
+    List<CampaignBrief> findByBrandId(Integer brandId);
+
+}
